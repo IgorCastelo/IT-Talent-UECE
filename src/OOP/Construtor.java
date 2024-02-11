a1b9259614c97de8596038e75962e04e63ae42e7
Igor Castelo <freitascasteloigor@gmail.com>
2024-02-11 17:51:59 -0300

    update referente ao conceito de construtores

---
+++ src/OOP/Construtor.java
@@ -0,0 +1,16 @@
+package OOP;
+
+public class Construtor {
+    public Integer idade;
+    public float saldo;
+    public String nome;
+
+    public Construtor(Integer idade, Float saldo, String nome){
+        this.idade=idade+10;
+        this.saldo=saldo+10;
+        this.nome=nome+ "Sobre nome";
+    }
+
+
+
+}
